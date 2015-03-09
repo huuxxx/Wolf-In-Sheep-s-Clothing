52777b1261919e23aee5dde740e6078713e45e23
whitey84 <109848@student.sae.edu.au>
2015-03-09 13:34:40 +1000

    test
    test

---
+++ Assets/Scripts/DiceMgr.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceMgr : MonoBehaviour {
+
+	private static DiceMgr _instance;
+
+	public static DiceMgr instance
+	{
+		get
+		{
+			if(_instance == null)
+				_instance = GameObject.FindObjectOfType<DiceMgr>();
+			return _instance;
+		}
+	}
+
+
+	public int Roll2D6() {
+		//ANTHONY TODO
+	}
+}
